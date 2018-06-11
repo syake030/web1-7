5b8cf9f2e1e9b2cb67f972f218044b062176bd10
Unknown <syake.letas1025@gmail.com>
2018-06-11 17:59:58 +0900

    cat

---
+++ ClimbCloud/Assets/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody2D rigid2D;
+    Animator animator;
+    float jumpForce = 680.0f;
+    float walkForce = 30.0f;
+    float maxWalkSpeed = 2.0f;
+	
+	void Start () {
+        this.rigid2D = GetComponent<Rigidbody2D>();
+        this.animator = GetComponent<Animator>();
+	}
+	
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space))
+        {
+            //ジャンプする
+            this.rigid2D.AddForce(transform.up * jumpForce);
+        }
+        //左右移動
+        int key = 0;
+        if (Input.GetKey(KeyCode.RightArrow)) key = 1;
+        if (Input.GetKey(KeyCode.LeftArrow)) key = -1;
+
+        //プレイヤーの速度
+        float speedx = Mathf.Abs(this.rigid2D.velocity.x);
+
+        //スピード制限
+        if (speedx < this.maxWalkSpeed)
+        {
+            this.rigid2D.AddForce(transform.right * key * this.walkForce);
+        }
+
+        if(key!=0)
+        {
+            transform.localScale = new Vector3(key, 1, 1);
+        }
+
+        //プレイヤーの速度に応じてアニメーション速度を変える
+        this.animator.speed = speedx / 2.0f;
+
+    }
+}
